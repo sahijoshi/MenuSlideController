82594e4fd30750b57ce4cbd70b242b1a3dd07c2f
Sahi Joshi <sahik.joshi@gmail.com>
2018-08-07 17:18:44 +0545

    add workspace and manage framework.

--- Example/MenuSlideController/CenterViewController.swift
+++ Example/MenuSlideController/CenterViewController.swift
@@ -12,13 +12,11 @@ class CenterViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .red
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     @IBAction func toggleLeft(_ sender: Any) {
---
+++ MenuSlideControllerSource/MenuSlideControllerSource/Sources/Segues/SideControllerSegue.swift
@@ -0,0 +1,21 @@
+//
+//  SideControllerSegue.swift
+//  MenuSlideController
+//
+//  Created by Sahi Joshi on 6/27/18.
+//  Copyright © 2018 Sahi Joshi. All rights reserved.
+//
+
+import UIKit
+
+open class SideControllerSegue: UIStoryboardSegue {
+    
+    override open func perform() {
+        if let sideController = self.source as? MenuSlideController {
+            sideController.add(sideViewController: destination)
+        } else {
+            fatalError("This type of segue must only be used from a SideMenuController")
+        }
+    }
+
+}
